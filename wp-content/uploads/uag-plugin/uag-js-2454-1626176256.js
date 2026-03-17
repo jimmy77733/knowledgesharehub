eaffd0d3b7c39b36764e37dd6722af5f4ec56f73
jimmy77733 <jimmy880702@gmail.com>
2026-03-17 16:17:22 +0800

    0317-BUILD

---
+++ wp-content/uploads/uag-plugin/uag-js-2454-1626176256.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function(){ 			jQuery( document ).ready(function() {
+				UAGBTableOfContents._run( {"mappingHeaders":[true,true,true,true,true,true],"scrollToTop":false}, '.uagb-block-e500c4f9' );
+			});
+						jQuery( document ).ready(function() {
+				UAGBTableOfContents._run( {"mappingHeaders":[true,true,true,true,true,true],"scrollToTop":false}, '.uagb-block-e500c4f9' );
+			});
+			 })
